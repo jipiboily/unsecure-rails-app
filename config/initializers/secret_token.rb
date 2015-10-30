5c69dd101f57d3669ea3814b5fdeea2d06dfd6d6
Jean-Philippe Boily <j@jipi.ca>
2015-10-30 14:03:33 -0400

    initial unsecure commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnsecureRailsApp::Application.config.secret_key_base = 'cd4df3dab154f43318e03e482755320fb0d6b6e9c9b1053d567d98730b5ddd037e3872ea5fe6b014028afa16b19d635639de891bbe5768481246d592f4ba611d'
